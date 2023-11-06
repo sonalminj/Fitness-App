3a3d17e54edcbde8ef2157d55f91687740bbcc0c
sonalminj <sonalminjprayas@gmail.com>
2023-11-06 21:56:38 +0530

    setup initial project structure

---
+++ src/components/HorizontalScrollbar.js
@@ -0,0 +1,27 @@
+import { Box, ListItemAvatar } from '@mui/material';
+import React from 'react';
+import BodyParts from './BodyParts';
+
+
+
+const HorizontalScrollbar = ({data ,bodyPart,setBodyPart}) => {
+  return (
+    <div>
+        {data.map((item)=> (
+        <Box 
+        key ={item.id||item}
+        itemId ={item.id||item}
+        title ={item.id||item}
+        m ="0 40px"
+        >
+          <BodyParts item = {item} bodyPart={bodyPart} 
+          setBodyPart={setBodyPart}/>
+
+        </Box>
+        ))}
+      
+    </div>
+  )
+}
+
+export default HorizontalScrollbar
