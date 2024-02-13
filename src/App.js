c2fcb4479655d5b9c965df1c77c980ce5b63f840
sonalminj <sonalminjprayas@gmail.com>
2024-02-13 13:49:58 +0530

    fix: set base route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 const App = () => (
   <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
     <Navbar />
-    <Routes>
+    <Routes >
       <Route path="/" element={<Home />} />
       <Route path="/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
