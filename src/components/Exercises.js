0eb8050c07027b3d5cf5d97ad5510948bde78339
sonalminj <sonalminjprayas@gmail.com>
2024-02-13 13:25:31 +0530

    fix: import defaultToArray

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { defaultToArray, exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 import Loader from './Loader';
 
